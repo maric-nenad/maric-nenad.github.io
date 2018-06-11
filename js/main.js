d5fedc74edbe4c628a4b556e1c49e5c53ac58851
Nenad Maric <nenad.maric@infobip.com>
2018-06-11 12:36:27 +0200

    Added about projects part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+
+    console.log('Welcome');
+
     $('[data-toggle="tooltip"]').tooltip(); 
+
+    $('.more-details-link').click(function() {
+        $(this).next('.more-details-content').toggle();
+
+        if ($(this).next('.more-details-content').is(':visible')) {
+            $(this).text('Less..')
+        } else {
+            $(this).text('More..')
+        }
+    });
 });
