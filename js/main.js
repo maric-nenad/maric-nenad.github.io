29023cebd8cbb94a697e272e46d406a87fd6836e
Nenad Maric <nenad.maric@infobip.com>
2018-07-27 22:07:22 +0200

    Prepare for print button, toggle short/long version without page refresh.

--- js/main.js
+++ js/main.js
@@ -14,12 +14,18 @@ $(document).ready(function(){
         }
     });
 
+    // Short version
     $('.dropdown-menu a:first-child').click(function(){   
+        if ($('.more-details-content').is(':visible')) {
+            $('.more-details-content').toggle();
+        }
         $('.more-details-link').css("visibility", "hidden");
+        $('.page-break-long').css("page-break-before", "auto");
     })
 
+    // Long version
     $('.dropdown-menu a:last-child').click(function(){   
-        $('.more-details-link').click();
+        $('.more-details-content').toggle();
         $('.more-details-link').css("visibility", "hidden");
         $('.page-break-long').css("page-break-before", "always");
     })
